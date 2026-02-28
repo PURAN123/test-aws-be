7d64f7988a0e9de40ecec3ea53313f9769a89451
puran123 <pchandra1002@gmail.com>
2026-03-01 03:15:10 +0530

    fix the nginx configuration

--- config/gunicorn_config.py
+++ config/gunicorn_config.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 # Gunicorn configuration for production
-bind = 'unix:/run/gunicorn.sock'
+bind = 'unix:/var/www/test-aws-be/test-aws-be/gunicorn.sock'
 workers = max(2, multiprocessing.cpu_count() * 2 + 1)
 worker_class = 'sync'
 threads = 2
